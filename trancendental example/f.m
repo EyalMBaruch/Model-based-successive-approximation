3aa15bf22af1c6ad2886ef34d01d2e2c02d0ff03
EyalMBaruch <127354428+EyalMBaruch@users.noreply.github.com>
2023-03-12 22:38:25 +0200

    Delete f.m

--- trancendental example/f.m
+++
@@ -1,7 +0,0 @@
-function wnsq=f(g)
-% this function uses the full system model f on the approximation g to evaluate the response wnsq
-% g - parameter approximation
-% x - control parameter. size of x will result in size of wnsq (no need for
-% this example)
-% wnsq - model output.
-wnsq=10*sin(15*g)+8*g+3;
