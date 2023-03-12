67bf311184470c4f3e90194ded7674e725da527e
EyalMBaruch <127354428+EyalMBaruch@users.noreply.github.com>
2023-03-12 22:16:02 +0200

    Delete fsinv.m

--- fsinv.m
+++
@@ -1,5 +0,0 @@
-function g=fsinv(w_tilde,x)
-% this function uses the inverse of the simplfied model fs to evaluate g 
-% w_tilde - perturbed measurments.
-% x - control parameter. size of x will result in size of g
-% g - parameter approximation
