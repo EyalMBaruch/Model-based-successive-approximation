26a0affa1b048bea8ca661cfae69e5aa6a5fe475
EyalMBaruch <127354428+EyalMBaruch@users.noreply.github.com>
2023-03-12 21:34:26 +0200

    Add files via upload

---
+++ f.m
@@ -0,0 +1,5 @@
+function wnsq=f(g,x)
+% this function uses the full system model f on the approximation g to evaluate the response wnsq
+% g - parameter approximation
+% x - control parameter. size of x will result in size of wnsq
+% wnsq - model output.
---
+++ fsinv.m
@@ -0,0 +1,5 @@
+function g=fsinv(w_tilde,x)
+% this function uses the inverse of the simplfied model fs to evaluate g 
+% w_tilde - perturbed measurments.
+% x - control parameter. size of x will result in size of g
+% g - parameter approximation
