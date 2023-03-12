7f43d97f7e49188b1ce6008d903aeb9f48b1db70
EyalMBaruch <127354428+EyalMBaruch@users.noreply.github.com>
2023-03-12 22:15:40 +0200

    Delete f.m

--- f.m
+++
@@ -1,5 +0,0 @@
-function wnsq=f(g,x)
-% this function uses the full system model f on the approximation g to evaluate the response wnsq
-% g - parameter approximation
-% x - control parameter. size of x will result in size of wnsq
-% wnsq - model output.
